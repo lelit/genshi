dabac0f5f4858d16341b9e04123201ae924c7730
Lele Gaifax <lele@metapensiero.it>
2016-11-26 13:01:44 +0100

    Fix Emacs coding signature

--- examples/bench/basic.py
+++ examples/bench/basic.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # Template language benchmarks
 #
 # Objective: Test general templating features using a small template
--- examples/bench/bigtable.py
+++ examples/bench/bigtable.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # Template language benchmarks
 #
 # Objective: Generate a 1000x10 HTML table as fast as possible.
